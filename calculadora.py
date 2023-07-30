f57c2c944c0b1153f5ca8a612fb303a56449d7d6
MARIANA RUBERT GRECCO <55103645+maryrubert@users.noreply.github.com>
2023-07-30 18:21:26 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+def calculator(n1, n2, op):
+    if(op=="+"): 
+        return n1+n2
+    if(op=="-"): 
+        return n1-n2
+    if(op=="*"): 
+        return n1*n2
+    if(op=="/"): 
+        return n1/n2
+    else: 
+        return 0
+
+print(calculator(4, 7, "**"))    
+
+  
